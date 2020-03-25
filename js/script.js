aafb8d4c8bb42161d24c4c553f0a3fbd158e6cc7
mdraza <raza..uidev@gmail.com>
2020-03-25 13:31:42 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,97 @@
+/*------------  Owl Carousal---------- */
+$('#owl-carousel1').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    dots: false,
+    autoplay: false,
+    autoplayTimeout: 2000,
+    responsive: {
+        0: {
+            items: 1.5
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+$('#owl-carousel2').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: false,
+    autoplayTimeout: 2500,
+    responsive: {
+        0: {
+            items: 1.5
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+$('#owl-carousel3').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: false,
+    autoplayTimeout: 2500,
+    responsive: {
+        0: {
+            items: 1.5
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
+
+$('#owl-carousel4').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: false,
+    autoplayTimeout: 2500,
+    responsive: {
+        0: {
+            items: 1.5
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
+$('#owl-carousel5').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: false,
+    autoplayTimeout: 2500,
+    responsive: {
+        0: {
+            items: 1.5
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
